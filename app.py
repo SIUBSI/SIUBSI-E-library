1d5bec7f3fd2b18acdf28749400ce689f8782db9
reighpuy <daffykhadaffy18@gmail.com>
2022-10-27 23:09:59 +0700

    Update (3.2)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import getpass, time, os
+import getpass, time, os # mengimport modul
 
+# LIST BERISI DAFTAR BUKU BESERTA DETAILNYA
 DaftarBuku = [
     # Page 1
     ["Pengantar teori dan algoritma graph", "220868", "Suryadi", "1990", "Indonesia", "Gunadarma"],
@@ -16,9 +17,9 @@ DaftarBuku = [
     # Page 2
     ["Logika dan algoritma dasar menggunakan bahasa C++", "210646", "Indarwoko Kurniadi", "2013", "Indonesia", "Mitra Wacana Media"],
     ["Konsep kecerdasan buatan", "210646", "Anita Desiani dan Muhammad Arhami", "2006", "Indonesia", "ANDI"],
-    ["Dasar - dasar pemrograman pascal ", "210646", "Abdul Kadir", "2007", "Indonesia", "ANDI"],
+    ["Dasar - dasar pemrograman pascal", "210646", "Abdul Kadir", "2007", "Indonesia", "ANDI"],
     ["Menguasai presentasi dengan microsoft powerpoint xp", "210646", "Triton Prawiro Budi", "2005", "Indonesia", "Tugu Publisher"],
-    ["36 jam belajar komputer microsoft office powerpoint 2003 ", "210646", "Budi Permana", "2005", "Indonesia", "PT Elex Media Komputindo"],
+    ["36 jam belajar komputer microsoft office powerpoint 2003", "210646", "Budi Permana", "2005", "Indonesia", "PT Elex Media Komputindo"],
     ["Troubleshooting windows xp : konsultasi dengan ahlinya", "210646", "Jubilee Enterprise", "2008", "Indonesia", "PT Elex Media Komputindo"],
     ["Microsoft windows me : sistem operasi masa depan", "210646", "Wali Eko Djatmiko", "2001", "Indonesia", "ANDI"],
     ["CISCO CCNA jaringan komputer", "210646", "Iwan Sofana", "2010", "Indonesia", "INFORMATIKA"],
@@ -26,15 +27,18 @@ DaftarBuku = [
     ["Teknik komputer jaringan (sistem operasi dan jaringan)", "210646", "Muhammad Badrul dkk", "2010", "Indonesia", "Inti Prima"],
 ]
 
+# ====== FUNGSI WELCOME MESSAGE ======
 def welcome():
     os.system("cls")
     print("\n"+"[    SIUBSI E-library    ]".center(60))
     print('"Menyediakan Book maupun E-book dikhususkan untuk Prodi Sistem Informasi pada Universitas Bina Sarana Informatika"\n\n')
 
+# ====== FUNGSI JEDA ======
 def jeda(durasijeda):
     time.sleep(durasijeda)
     os.system("cls")
 
+# ====== FUNGSI BUKU ======
 def daftarbuku(pagenum):
     if pagenum == 1:
         pagenum = DaftarBuku[0:10]
@@ -52,7 +56,22 @@ def daftarbuku(pagenum):
         num+=1
         print(f"{num}.\t{i[1]}\t\t{i[0]}")
 
-
+def detailbuku(kodebuku):
+    for i in DaftarBuku[0:20]:
+        if kodebuku in i[1]:
+            return print(f"""
+            Kode Buku: {i[1]}
+            Judul Buku: {i[0]}
+            Penulis: {i[2]}
+            Tahun: {i[3]}
+            """, end="")
+        else:
+            pass
+    else:
+        print(f"{kodebuku} tidak ditemukan")
+    
+        
+# ====== FUNGSI USER LOGIN ======
 def masuk(Username, Password):
     Sukses = False
     file = open("Data/users.txt", "r")
@@ -71,27 +90,34 @@ def masuk(Username, Password):
         os.system("cls")
         daftarbuku(1)
         while True:
-            Halaman = input("\nPilih nomor untuk dialihkan ke halaman nomor tersebut, masukkan 'exit' agar dialihkan ke Menu [1|2|exit]: ")
+            Halaman = input("\nPilih nomor untuk dialihkan ke halaman nomor tersebut, Masukkan 'menu' agar dialihkan ke Menu buku, Masukkan 'exit' untuk keluar dari program.\n[1|2|menu]: ")
         
             if Halaman == "1":
                 daftarbuku(1)
             elif Halaman == "2":
                 daftarbuku(2)
-            elif Halaman == "exit":
-                RedirectMenuBuku = input("[1]. Pinjam buku\n[2]. Pindah halaman\n[3]. Cari buku")
+            elif Halaman == "menu":
+                print("-"*30)
+                print("\n[1]. Pinjam buku\n[2]. Lihat detail buku")
+                RedirectMenuBuku = input("Masukkan pilihan [1|2]: ")
 
                 if RedirectMenuBuku == "1":
                     print("Pinjam Buku")
                 elif RedirectMenuBuku == "2":
-                    print("Pindah halaman")
-                elif RedirectMenuBuku == "3":
-                    print("Cari buku")
+                    kodenya = input("Masukkan Kode Buku: ")
+                    detailbuku(kodenya)
+                else:
+                    print(f"Pilihan {RedirectMenuBuku} tidak ditemukan")
+            elif Halaman == "exit":
+                jeda(1)
+                exit()
             else:
                 print("Nomor Halaman tidak tersedia.")
 
     else:
         print("Anda belum terdaftar, silahkan daftar terlebih dahulu!")
 
+# ====== FUNGSI USER REGISTER ======
 def daftar(Username, Password):
     file = open("Data/users.txt", "r")
     if os.stat("Data/users.txt").st_size == 0:
@@ -101,40 +127,42 @@ def daftar(Username, Password):
         file = open("Data/users.txt", "a")
         file.write(f"\n{Username}"+f"#{Password}")
 
+# ====== FUNGSI USER ACCESS ======
 def access(opsi):
     global Username
     if opsi == "1":
-        jeda(1)
-        print("\n[Pesan] Masukkan Username dan Password akun anda dengan benar.")
-        Username = input("Masukkan Username anda: ")
-        Password = getpass.getpass("Masukkan Password anda: ")
-        masuk(Username, Password)
-    elif opsi == "2":
         jeda(1,)
         print("\n[Pesan] Masukkan Username dan Password untuk akun anda.")
         Username = input("Masukkan Username: ")
         Password = getpass.getpass("Masukkan Password: ")
-        daftar(Username, Password)
-
         print("\nMohon tunggu sebentar...")
         jeda(1)
-        print(f"[Pesan] Berhasil membuat akun dengan Username {Username}, silahkan Masuk!\n")
+        print(f"[Pesan] Berhasil membuat akun dengan Username {Username}, Silahkan Masuk!")
+        daftar(Username, Password)
 
-        print("[Pesan] Masukkan Username dan Password akun anda dengan benar.")
+        print("[Pesan] Masukkan Username dan Password akun anda dengan benar.\n")
+        Username = input("Masukkan Username anda: ")
+        Password = getpass.getpass("Masukkan Password anda: ")
+        masuk(Username, Password)
+    elif opsi == "2":
+        jeda(1)
+        print("\n[Pesan] Masukkan Username dan Password akun anda dengan benar.")
         Username = input("Masukkan Username anda: ")
         Password = getpass.getpass("Masukkan Password anda: ")
         masuk(Username, Password)
     else:
         print(f"Menu {opsi} tidak diketahui.")
 
+# ====== FUNGSI MEMULAI PROGRAM ======
 def mulai():
     global opsi
     welcome()
-    print("[1] Masuk (Silahkan pilih menu ini jika anda sudah mempunyai akun).")
-    print("[2] Daftar (Silahkan pilih menu ini jika anda belum mempunyai akun).")
+    print("[1] Daftar (Silahkan pilih menu ini jika anda belum mempunyai akun).")
+    print("[2] Masuk (Silahkan pilih menu ini jika anda sudah mempunyai akun).")
     opsi = input("Masukkan pilihan [1|2]: ")
     if opsi != "1" and opsi != "2":
         mulai()
 
+# ====== MEMANGGIL FUNGSI MEMULAI PROGRAM BESERTA ACCESS-NYA ======
 mulai()
 access(opsi)
